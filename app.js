4583f0aa9d12267e3aaf7c5ba4c978984aab704d
Himanshu <himanshumuj11@gmail.com>
2019-05-05 21:12:20 +0530

    add note form added , server side validation added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const app = express();
@@ -24,6 +25,8 @@ app.engine('handlebars', exphbs({
 }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 const port = 5000;
  
 //Index route
@@ -37,6 +40,35 @@ app.get('/', (req,res) => {
 //About route
 app.get('/about', (req,res) => {
     res.render('about');
+});
+
+app.get('/ideas/add', (req,res) => {
+    res.render('ideas/add');
+});
+
+app.post('/ideas', (req,res) => {
+    let errors = [];
+
+    if(!req.body.title){
+        errors.push({
+            text:'Please add a title'
+        });
+    }
+    if(!req.body.details){
+        errors.push({
+            text:'Please add some details'
+        });
+    }
+
+    if(errors.length > 0){
+        res.render('ideas/add', {
+            errors: errors,
+            title : req.body.title,
+            details : req.body.details
+        });
+    }else {
+        res.send('passed');
+    }
 });
 
 app.listen(port, () => {
