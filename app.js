5dcbd34f8ee6cd1e3defc2c516e10dc80f931c41
Himanshu <himanshumuj11@gmail.com>
2019-05-06 21:16:09 +0530

    edit and update request added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
 const mongoose = require('mongoose');
 
 const app = express();
@@ -27,6 +28,10 @@ app.set('view engine', 'handlebars');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+//method-override middleware
+app.use(methodOverride('_method'))
+
 const port = 5000;
 
 //Index route
@@ -102,6 +107,24 @@ app.post('/ideas', (req, res) => {
                 res.redirect('/ideas')
             })
     }
+});
+
+
+//edit form process (PUT request)
+app.put('/ideas/:id', (req, res) => {
+    Idea.findOne({
+        _id: req.params.id
+    })
+        .then(idea => {
+            //new values
+            idea.title = req.body.title;
+            idea.details = req.body.details;
+
+            idea.save()
+                .then(idea => {
+                    res.redirect('/ideas');
+                })
+        })
 });
 
 app.listen(port, () => {
