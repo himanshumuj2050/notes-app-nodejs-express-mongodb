1986ccbab7f92a3c7d1792613be2a64a46d5eb0a
Himanshu <himanshumuj11@gmail.com>
2019-05-07 17:12:24 +0530

    users schema added, validation added in registration form

--- routes/users.js
+++ routes/users.js
@@ -12,4 +12,32 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
+//Register form POST route
+router.post('/register' , (req,res) => {
+    let errors = [];
+
+    if(req.body.password != req.body.password2){
+        errors.push({
+            text: 'Password mudo not match'
+        });
+    }
+
+    if(req.body.password.length < 4){
+        errors.push({
+            text: 'Password must be atleast 4 characters'
+        });
+    }
+
+    if(errors.length > 0){
+        res.render('users/register', {
+            errors: errors,
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            password2: req.body.password2
+        });
+    }else{
+        res.send('passed');
+    }
+});
 module.exports = router;
