32416e5ef3ecb6658def76353cc2b7e8d6d2a671
Himanshu <himanshumuj11@gmail.com>
2019-05-07 18:53:46 +0530

    login and logout functionality for users

--- config/passport.js
+++ config/passport.js
@@ -9,8 +9,40 @@ module.exports = function(passport){
     passport.use(new LocalStrategy({
         usernameField: 'email'
     }, (email, password, done) => {
-        console.log(email);
+        //match user
+        User.findOne({
+            email: email
+        }).then(user => {
+            if(!user){
+                return done(null, false, {
+                    message: 'No user found'
+                });
+            }
+
+            //password match for login
+            bcrypt.compare(password, user.password, (err, isMatch) => {
+                if(err) throw err;
+                if(isMatch){
+                    return done(null, user);
+                }else{
+                    return done(null, false, {
+                        message: 'Incorrect Password'
+                    });
+                }
+            })
+        })
     }));
+
+    //serailize and deserialize
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+      });
+      
+      passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+          done(err, user);
+        });
+      });
 }
 
 
